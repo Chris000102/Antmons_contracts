d158b3dc7af9a592d11cd97d9484bfacc0c0056a
Chris000102 <mullar000102@outlook.com>
2022-05-07 17:21:02 +0800

    add nft cap

--- hero.sol
+++ hero.sol
@@ -1,12 +1,15 @@
 pragma solidity ^0.8.4;
 import "./lib/nftBase.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract hero is antmonsBase{
+    using SafeMath for uint;
     constructor()ERC721("Antmons NFT","Antmons NFT"){
         _grantRole(DEFAULT_ADMIN_ROLE,_msgSender());
         _grantRole(MINT_ROLE,_msgSender());
     }
 
     function SpecificMint(address to,uint id)public onlyRole(MINT_ROLE){
+        require(totalSupply().add(1)<=10000,"EXCEED MINT LIMIT");
         specificMint(to,id);
     }
 }
--- lootbox.sol
+++ lootbox.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.8.4;
 import "./lib/nftBase.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 contract lootBox is antmonsBase{
-    constructor(string memory name,string memory symbol)ERC721(name,symbol){//Antmons NFT Normal/Rare/Epic
+    using SafeMath for uint;
+    uint public _mintLimit;
+    constructor(string memory name,string memory symbol,uint limit)ERC721(name,symbol){//Antmons NFT Normal/Rare/Epic
         _id = 1;
         _grantRole(DEFAULT_ADMIN_ROLE,_msgSender());
         _grantRole(MINT_ROLE,_msgSender());
+        _mintLimit = limit;
     }
 
     function BatchMint(address to,uint amount)public onlyRole(MINT_ROLE){//
+        require(totalSupply().add(amount)<=_mintLimit,"EXCEED MINT LIMIT");
         batchMint(to,amount);
     }
 }
